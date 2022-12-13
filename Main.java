78b188cac1c7cc7875a8a4c09de0db1631c65196
iamevs <iamevs143@gmail.com>
2022-12-13 12:20:17 +0530

    🪲 Bugs fixed

--- Main.java
+++ Main.java
@@ -1,89 +1,89 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package miniproject;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+public class Main {
 
-/**
- *
- * @author sanjay
- */
-class DrugWareHouse{
-    public static ArrayList<String> medicines = new ArrayList<String>();
-    Scanner s = new Scanner(System.in);
-    public void medicine(){
+    public static void operations() {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> medicines = new ArrayList<String>();
         medicines.add("Paracetemol");
         medicines.add("Betadine");
         medicines.add("Pantacid");
-        
         System.out.println("1. List the available medicines");
         System.out.println("2. Check medicine availability");
         System.out.println("3. Add medicine");
         System.out.println("4. Remove medicine");
-        System.out.println("ENTER YOUR CHOICE");
+        System.out.println("\n\nCtrl+C to exit");
+        System.out.print("ENTER YOUR CHOICE : ");
         int choice;
-        choice = s.nextInt();
-        switch(choice){
+        choice = sc.nextInt();
+        switch (choice) {
             case 1:
                 int size = medicines.size();
+                System.out.print("\033[H\033[2J");
                 System.out.println("The Available medicines are:");
-                for(int i = 0; i< size;i++){
+                for (int i = 0; i < size; i++) {
                     System.out.println(medicines.get(i));
-                
-            }break;
-            case 2:
-                System.out.println("Enter the medicine name");
-                String med = s.next();
-                if(medicines.contains(med)){
-                    System.out.println(med+" is Available");
-                }else{
-                    System.out.println(med+" is unavailable");
                 }
-            break;
+                System.out.println("\n\n");
+                break;
+            case 2:
+                System.out.print("\033[H\033[2J");
+                System.out.println("Enter the medicine name");
+                String med = sc.next();
+                if (medicines.contains(med)) {
+                    System.out.println(med + " is Available");
+                } else {
+                    System.out.println(med + " is unavailable");
+                }
+                System.out.println("\n\n");
+                break;
             case 3:
+                System.out.print("\033[H\033[2J");
                 System.out.println("Enter the medicine name:");
-                String med1 = s.next();
-                if(medicines.contains(med1)){
+                String med1 = sc.next();
+                if (medicines.contains(med1)) {
                     System.out.println("Medicine is already available");
-                }else{
+                } else {
                     medicines.add(med1);
-                }int size1 = medicines.size();
+                }
+                int size1 = medicines.size();
                 System.out.println("The  medicines are:");
-                for(int i = 0; i< size1;i++){
+                for (int i = 0; i < size1; i++) {
                     System.out.println(medicines.get(i));
-                
-            }
-            break;
+                }
+                System.out.println("\n\n");
+                break;
             case 4:
+                System.out.print("\033[H\033[2J");
                 System.out.println("Enter the name of the medicine to be removed:");
-                String med2 = s.next();
-                if(medicines.contains(med2)){
+                String med2 = sc.next();
+                if (medicines.contains(med2)) {
                     medicines.remove(med2);
-                }else{
-                    System.out.println("Medicine is unavailable");
-                }int size2 = medicines.size();
+                } else {
+                    System.out.println("Medicine is not available");
+                }
+                int size2 = medicines.size();
                 System.out.println("The  medicines are:");
-                for(int i = 0; i< size2;i++){
+                for (int i = 0; i < size2; i++) {
                     System.out.println(medicines.get(i));
-                
-            }
-            break;
-                
-                
+                }
+                System.out.println("\n\n");
+                break;
+            default:
+                System.out.print("\033[H\033[2J");
+                System.out.println("Invalid choice");
+                System.out.println("\n\n");
         }
     }
-}
-public class Miniproject {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        DrugWareHouse obj1 = new DrugWareHouse();
-        obj1.medicine();
+        while (true) {
+            operations();
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
     }
-    
 }
